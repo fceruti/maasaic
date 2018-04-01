1e67ceb7516bec580f5fca9aaa798d89b9848927
Francisco Ceruti <francisco.ceruti@gmail.com>
2018-04-02 05:16:09 +0700

    Add loading

---
+++ maasaic/assets/js/loading.js
@@ -0,0 +1,10 @@
+function showLoading() {
+    $('#loading-overlay').removeClass('hidden');
+}
+
+function hideLoading () {
+    $('#loading-overlay').addClass('hidden');
+}
+
+EventBus.subscribe(WAITING_SERVER_RESPONSE_STARTED, showLoading);
+EventBus.subscribe(WAITING_SERVER_RESPONSE_ENDED, hideLoading);
