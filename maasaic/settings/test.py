f539e6616c0ff410961f0a16188fad18c349a7d1
Francisco Ceruti <francisco.ceruti@gmail.com>
2018-04-14 17:36:47 +0700

    Add pytest

---
+++ maasaic/settings/test.py
@@ -0,0 +1,15 @@
+from maasaic.settings.common import *
+
+DEBUG = False
+TEMPLATE_DEBUG = False
+
+PASSWORD_HASHERS = [
+    'django.contrib.auth.hashers.MD5PasswordHasher',
+]
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.sqlite3',
+#         'NAME': 'test_db'
+#     }
+# }
+EMAIL_BACKEND = 'django.core.mail.backends.locmem.EmailBackend'
