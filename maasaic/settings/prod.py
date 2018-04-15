201257e71ce19f42068ea2c43f2d94bda0f8c3f8
Francisco Ceruti <francisco.ceruti@gmail.com>
2018-04-16 03:50:31 +0700

    Added production logging

--- maasaic/settings/prod.py
+++ maasaic/settings/prod.py
@@ -20,3 +20,43 @@ STATIC_URL = 'https://%s.s3.amazonaws.com/%s/' % (AWS_S3_CUSTOM_DOMAIN, AWS_LOCA
 
 COMPRESS_ENABLED = True
 COMPRESS_OFFLINE = True
+
+# ------------------------------------------------------------------------------
+# Logging
+# ------------------------------------------------------------------------------
+LOGGING = {
+    'version': 1,
+    'disable_existing_loggers': True,
+    'root': {
+        'level': 'INFO',
+        'handlers': ['console'],
+    },
+    'formatters': {
+        'standard': {
+            'format': '[%(levelname)7s] %(asctime)s  %(name)20s %(lineno)3d | %(message)s',
+
+        },
+    },
+    'handlers': {
+        'log_file': {
+            'level': 'INFO',
+            'class': 'logging.handlers.RotatingFileHandler',
+            'filename': os.path.join('/var/log/django', 'info.log'),
+            'maxBytes': 1024 * 1024 * 15,  # 15MB
+            'backupCount': 10,
+            'formatter': 'standard',
+        },
+        'console': {
+            'level': 'DEBUG',
+            'class': 'logging.StreamHandler',
+            'formatter': 'standard',
+        }
+    },
+    'loggers': {
+        'django.request': {
+            'handlers': ['log_file', 'console'],
+            'level': 'INFO',
+            'propagate': True,
+        },
+    }
+}
