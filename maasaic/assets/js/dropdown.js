a9dc8e76ab96cb8c60bb1881e8ead7bc67d89cdc
Francisco Ceruti <francisco.ceruti@gmail.com>
2018-04-20 10:01:01 +0700

    Fix dropdowns

--- maasaic/assets/js/dropdown.js
+++ maasaic/assets/js/dropdown.js
@@ -1,31 +1,22 @@
 function DropDown(el) {
     this.dd = el;
-    this.placeholder = this.dd.children('span');
-    this.opts = this.dd.find('ul.sidebar-dropdown > li');
-    this.val = '';
-    this.index = -1;
+    this.isInside = false;
     this.initEvents();
 }
 DropDown.prototype = {
     initEvents : function() {
         var obj = this;
-
         obj.dd.on('click', function(event){
             $(this).toggleClass('active');
             return false;
         });
-
-        obj.opts.on('click',function(){
-            var opt = $(this);
-            obj.val = opt.text();
-            obj.index = opt.index();
-            obj.placeholder.text(obj.val);
+        obj.dd.on('mouseenter', function(){
+            this.isInside = true;
+        });
+        obj.dd.on('mouseleave', function(){
+            $(this).removeClass('active');
+            this.isInside = false;
         });
     },
-    getValue : function() {
-        return this.val;
-    },
-    getIndex : function() {
-        return this.index;
-    }
 }
+
