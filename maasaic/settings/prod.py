733d6e4b04ac6f873d48362c747fc56ab4482072
Francisco Ceruti <francisco.ceruti@gmail.com>
2018-05-04 16:24:21 +1000

    Add media url to prod settings

--- maasaic/settings/prod.py
+++ maasaic/settings/prod.py
@@ -5,6 +5,7 @@ from maasaic.settings.common import *
 # Static files
 # ------------------------------------------------------------------------------
 STATIC_URL = 'https://%s/%s/' % (AWS_S3_CUSTOM_DOMAIN, AWS_LOCATION)
+MEDIA_URL = 'https://%s/%s/' % (AWS_S3_CUSTOM_DOMAIN, 'media')
 COMPRESS_URL = STATIC_URL
 
 DEFAULT_FILE_STORAGE = 'maasaic.apps.utils.storage_backends.S3MediaStorage'
