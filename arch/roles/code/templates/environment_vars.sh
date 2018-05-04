13d08c1a165e0330d54893b8589405ae084fd3bc
Francisco Ceruti <francisco.ceruti@gmail.com>
2018-05-04 12:46:08 +1000

    Add write environment keys to deploy script

--- arch/roles/code/templates/environment_vars.sh
+++ arch/roles/code/templates/environment_vars.sh
@@ -4,6 +4,8 @@ export DJANGO_SECRET_KEY={{ django_secret_key }}
 export DJANGO_DATABASE_URL=postgres://{{ django_db_user }}:{{ django_db_password }}@{{ django_db_host }}:5432/{{ django_db_name }}
 export DEFAULT_SITE_DOMAIN=maasaic.com
 
+export GIPHY_KEY={{ giphy_key }}
+
 export AWS_ACCESS_KEY_ID={{ aws_access_key_id }}
 export AWS_SECRET_ACCESS_KEY={{ aws_secret_key }}
 export AWS_STORAGE_BUCKET_NAME={{ s3_bucket_name }}
